34154cdeee412ed2ff74c63712d94a2a01964ab8
Mateusz Konczal <mateuszkonczal96@gmail.com>
2019-10-06 15:23:11 +0200

    Modified car colours

--- src/main/java/pl/edu/utp/wtie/homeworkweek3/model/CarColour.java
+++ src/main/java/pl/edu/utp/wtie/homeworkweek3/model/CarColour.java
@@ -1,22 +1,12 @@
 package pl.edu.utp.wtie.homeworkweek3.model;
 
 public enum CarColour {
-    BLACK("czarny"),
-    BLUE("niebieski"),
-    GREEN("zielony"),
-    PINK("różowy"),
-    RED("czerwony"),
-    SILVER("srebrny"),
-    WHITE("biały"),
-    YELLOW("zółty");
-
-    private String polishName;
-
-    CarColour(String polishName) {
-        this.polishName = polishName;
-    }
-
-    public String getPolishName() {
-        return polishName;
-    }
+    BLACK,
+    BLUE,
+    GREEN,
+    PINK,
+    RED,
+    SILVER,
+    WHITE,
+    YELLOW;
 }
